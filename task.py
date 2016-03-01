aed1cca31cd660ab8af8782b323c60c9ba5f4b30
Helga-ok <solntce30@mail.ru>
2016-03-02 00:36:39 +0300

    just refactoring code

--- task.py
+++ task.py
@@ -7,11 +7,21 @@ text = open(input_file).read()
 
 
 def clear_text(text_for_clear):
-	clear_text = re.sub(r'[\n\t\r\.\(\)\{\}\[\]\+\?\!\*\"\'«»%:,–]', " ", text_for_clear, flags = re.U | re.I | re.M)
+	clear_text = re.sub(r'[\n\t\r\.\(\)\{\}\[\]\+\?\!\*\"\'«»%:;,–]', " ", text_for_clear, flags = re.U | re.I | re.M)
 	return clear_text
 
 def get_normal_form(word):
 	return morph.parse(word)[0].normal_form
+
+def save_indexies(dictionary):
+	output_file = open('indexes_of_' + str(input_file) + '.txt', 'w')
+	for item in dictionary:
+		output_file.write(str(item[0]))
+		for idx in item[1]:
+			output_file.write(' ' + str(idx))
+		output_file.write('\n')
+	output_file.close()
+
 
 def main():
 	dictionary = dict()
@@ -24,7 +34,7 @@ def main():
 	for line in input_text.read().split('\n'):
 		clean_text = clear_text(line)
 		for word in clean_text.split(' '):
-			if word is not '':
+			if word is not '' and "--":
 				count_of_tokens += 1
 				count_of_chars_token += len(word)
 				normal_word = get_normal_form(word)
@@ -38,16 +48,10 @@ def main():
 		i += 1
 	input_text.close()
 	d = sorted(dictionary.items())
-	output_file = open('indexes.txt', 'w')
-	for item in d:
-		output_file.write(str(item[0]))
-		for v in item[1]:
-			output_file.write(' ' + str(v))
-		output_file.write('\n')
-	output_file.close()
+	save_indexies(d)
 	count_of_terms = len(d)
 	print('Количество токенов: ' + repr(count_of_tokens))
 	print('Количество терминов: ' + repr(count_of_terms))
-	print('Средняя длина токена: ' + repr(count_of_chars_token//count_of_tokens))	
-	print('Средняя длина термина: ' + repr(count_of_chars_term//count_of_terms))
+	print('Средняя длина токена: ' + repr(count_of_chars_token/count_of_tokens))	
+	print('Средняя длина термина: ' + repr(count_of_chars_term/count_of_terms))
 main()
